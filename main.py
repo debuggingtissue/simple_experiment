434bb51d91c1501be31d421826e71d4f68a43f22
debuggingtissue <debugger@debuggingtissue.com>
2020-11-28 17:48:45 +0100

    add experiment 2 and density dataset

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ def clear_pyplot_memory():
     plt.cla()
     plt.close()
 
-
-if __name__ == '__main__':
+def experiment_1():
     path = untar_data(URLs.PETS)
     print(path.ls())
     files = get_image_files(path / "images")
@@ -35,5 +34,41 @@ if __name__ == '__main__':
     learn.show_results()
     plt.savefig(f'plot_predictions.png')
     clear_pyplot_memory()
+
+def experiment_2():
+    # path = untar_data(URLs.PETS)
+    # print(path.ls())
+    path = "dataset"
+    files = get_image_files("dataset")
+
+    data_block = DataBlock(blocks=(ImageBlock, CategoryBlock),
+                           get_items=get_image_files,
+                           splitter=RandomSplitter(),
+                           get_y=parent_label,
+                           item_tfms=Resize(224))
+    dls = data_block.dataloaders(Path(path), bs=10)
+    dls.show_batch()
+    plt.savefig(f'show_batch.png')
+    clear_pyplot_memory()
+
+    learn = cnn_learner(dls, resnet18, metrics=error_rate)
+    learn.fine_tune(10)
+    learn.recorder.plot_loss()
+    plt.savefig(f'plot_loss.png')
+    clear_pyplot_memory()
+
+    learn.predict(files[0])
+    learn.show_results()
+    plt.savefig(f'plot_predictions.png')
+    clear_pyplot_memory()
+
+
+if __name__ == '__main__':
+   print("hey")
+   experiment_2()
+
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
