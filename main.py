1fbc34112dc184e47a25769cd519720c35343807
debuggingtissue <debugger@debuggingtissue.com>
2020-12-12 22:26:44 +0100

    upload results

--- main.py
+++ main.py
@@ -338,8 +338,6 @@ def save_plots(learn, epoch_nr, output_directory, dls=None):
     plt.savefig(f'{output_directory}/show_batch.png')
     clear_pyplot_memory()
 
-
-
     # img = PILImage.create("cid=TCGA-CH-5763-01Z-00-DX1.7d4eff47-8d99-41d4-87f0-163b2cb034bf###rl=0###x=95204###y=24800###w=800###h=800###pnc=171.png")
     # x, = first(dls.test_dl([img]))
     #
@@ -418,19 +416,28 @@ def save_plots(learn, epoch_nr, output_directory, dls=None):
     # plt.savefig(f'other_layer_activation_plot.png')
     # clear_pyplot_memory()
 
-    def generate_data_block(self):
-        data_block = DataBlock(blocks=(ImageBlock, CategoryBlock),
-                               get_items=get_image_files,
-                               splitter=RandomSplitter(),
-                               get_y=parent_label,
-                               item_tfms=self.transforms["item"],
-                               batch_tfms=self.transforms["batch"])
 
-        return data_block
+def generate_data_block(self):
+    dat_block = DataBlock(blocks=(ImageBlock, CategoryBlock),
+                          get_items=get_image_files,
+                          splitter=RandomSplitter(),
+                          get_y=parent_label,
+                          item_tfms=self.transforms["item"],
+                          batch_tfms=self.transforms["batch"])
 
-    def dataloaders(self, dataset_path, batch_size):
-        dls = self.data_block.dataloaders(Path(dataset_path), bs=batch_size)
-        return dls
+    return data_block
+
+
+def dataloaders(self, dataset_path, batch_size):
+    dls = self.data_block.dataloaders(Path(dataset_path), bs=batch_size)
+    return dls
+
+
+def create_experiment_images(experiment_result_directory_paths, output_directory):
+    for experiment_result_directory_path in experiment_result_directory_paths:
+        image_merger.ImageMerger.create_overview_image(experiment_result_directory_path, output_directory)
+
+
 if __name__ == '__main__':
     # experiment_1a([5, 10, 30, 50, 100, 500], "e_1a")
     # experiment_1b([5, 10, 30, 50, 100, 500], "e_1b")
@@ -439,7 +446,8 @@ if __name__ == '__main__':
     # experiment_2c([5, 10, 30, 50, 70, 100, 500], "e_2c")
     # experiment_3a([5, 10, 30, 50, 70, 100, 500], "e_3a")
     # experiment_3b([5, 10, 30, 50, 70, 100, 500], "e_3b")
-    image_merger.ImageMerger.create_overview_image("e_2c", "lol")
-
+    # image_merger.ImageMerger.create_overview_image("e_2c", "lol")
+    create_experiment_images(
+        ["e_1a_thesis", "e_1b_thesis", "e_2a_thesis", "e_2b_thesis", "e_2c_thesis", "e_3a_thesis", "e_3b_thesis"], "overview_images")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
